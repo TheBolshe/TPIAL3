10d34ce3c674b3d439d9e8233a36a7bf9cd93bf3
Guynko <guy.krakowiak@live.com>
2020-02-06 17:47:40 +0100

    Exo 1.2 : Heuristique (ca a l'air bon)

--- src/main/java/jeux/dominos/HeuristiquesDominos.java
+++ src/main/java/jeux/dominos/HeuristiquesDominos.java
@@ -7,18 +7,52 @@ import iia.jeux.modele.joueur.Joueur;
 
 public class HeuristiquesDominos{
 
-	public static  Heuristique hblanc = new Heuristique(){
-				
+	public static Heuristique hblanc = new Heuristique(){
+
 		public int eval(PlateauJeu p, Joueur j){
-			return ((PlateauDominos) p).nbCoupsBlanc() - ((PlateauDominos) p).nbCoupsNoir();
+			if (p.isJoueurBlanc(j)) {
+				if (p.nbCoupsBlanc() == 0) {
+					return Integer.MIN_VALUE;
+				}
+				if (p.nbCoupsNoir() == 0) {
+					return Integer.MAX_VALUE;
+				}
+				return ((PlateauDominos) p).nbCoupsBlanc() - ((PlateauDominos) p).nbCoupsNoir();
+			}
+			else {
+				if (p.nbCoupsNoir() == 0) {
+					return MAX_VALUE;
+				}
+				if (p.nbCoupsBlanc() == 0) {
+					return MIN_VALUE;
+				}
+				return ((PlateauDominos) p).nbCoupsBlanc() - ((PlateauDominos) p).nbCoupsNoir();
+			}
 		}
 	};
 
 	public static  Heuristique hnoir = new Heuristique(){
-	
+
 		public int eval(PlateauJeu p, Joueur j){
-			return ((PlateauDominos) p).nbCoupsNoir() - ((PlateauDominos) p).nbCoupsBlanc();
-		}
-	};
+			if (p.isJoueurNoir(j)) {
+				if (p.nbCoupsBlanc() == 0) {
+					return Integer.MAX_VALUE;
+				}
+				if (p.nbCoupsNoir() == 0) {
+					return Integer.MIN_VALUE;
+				}
+				return ((PlateauDominos) p).nbCoupsNoir() - ((PlateauDominos) p).nbCoupsBlanc();
+			}
+			else {
+				if (p.nbCoupsNoir() == 0) {
+					return MIN_VALUE;
+				}
+				if (p.nbCoupsBlanc() == 0) {
+					return MAX_VALUE;
+				}
+				return ((PlateauDominos) p).nbCoupsNoir() - ((PlateauDominos) p).nbCoupsBlanc();
+			}
+	}
+};
 
 }
